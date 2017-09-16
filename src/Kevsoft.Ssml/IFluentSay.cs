5d4afe9ff885ad4b050448902e59e2e049b6aa60
kevbite <kev_bite@msn.com>
2017-09-16 22:27:16 +0100

    retargeting to netstandard1.0

--- src/Kevsoft.Ssml/IFluentSay.cs
+++ src/Kevsoft.Ssml/IFluentSay.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Globalization;
-using System.Threading.Tasks;
-using System.Xml;
 
 namespace Kevsoft.Ssml
 {
@@ -16,45 +13,5 @@ namespace Kevsoft.Ssml
         ISsml AsTelephone();
 
         IFluentSayAsCharaters AsCharacters();
-    }
-
-    public interface IFluentSayNumber
-    {
-        ISsml AsCardinalNumber();
-
-        ISsml AsOrdinalNumber();
-    }
-
-    public class FluentSayNumber : IFluentSayNumber, ISsmlWriter
-    {
-        private readonly Ssml _ssml;
-        private readonly int _value;
-        private SayAsWriter _writer;
-
-        public FluentSayNumber(Ssml ssml, int value)
-        {
-            this._ssml = ssml;
-            this._value = value;
-        }
-
-        public ISsml AsCardinalNumber()
-        {
-            _writer = new SayAsWriter("cardinal", _value.ToString(CultureInfo.InvariantCulture));
-
-            return _ssml;
-        }
-
-        public ISsml AsOrdinalNumber()
-        {
-            _writer = new SayAsWriter("ordinal", _value.ToString(CultureInfo.InvariantCulture));
-
-            return _ssml;
-        }
-
-        public async Task WriteAsync(XmlWriter writer)
-        {
-            await _writer.WriteAsync(writer)
-                .ConfigureAwait(false);
-        }
     }
 }
