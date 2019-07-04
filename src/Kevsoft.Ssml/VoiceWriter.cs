ddd6fb716db9e20470e525a234d2ee2a7a47dbfb
Jon Masters <jon@cakeboss.com>
2019-07-04 12:06:12 -0500

    Adds support for AsVoice. Documentation at : https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/speech-synthesis-markup

---
+++ src/Kevsoft.Ssml/VoiceWriter.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using System.Xml;
+
+namespace Kevsoft.Ssml
+{
+    public class VoiceWriter : ISsmlWriter
+    {
+        private readonly ISsmlWriter _innerWriter;
+        private readonly string _name;
+
+        public VoiceWriter(ISsmlWriter innerWriter, string name)
+        {
+            _innerWriter = innerWriter;
+            _name = name;
+        }
+
+        public async Task WriteAsync(XmlWriter writer)
+        {
+            await writer.WriteStartElementAsync(null, "voice", null)
+                .ConfigureAwait(false);
+
+            await writer.WriteAttributeStringAsync(null, "name", null, _name)
+                .ConfigureAwait(false);
+
+            await _innerWriter.WriteAsync(writer)
+                .ConfigureAwait(false);
+
+            await writer.WriteEndElementAsync()
+                .ConfigureAwait(false);
+        }
+    }
+}
